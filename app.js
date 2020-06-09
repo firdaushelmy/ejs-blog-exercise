a4393ae52eb21977c989f9b10d8b93d25de279ab
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-10 02:06:17 +0800

    render variable homeStartingContent to home.ejs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.render(__dirname + '/views/home.ejs')
+  res.render('home', { homeStartingContent: homeStartingContent })
 })
 
 
