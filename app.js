0a6ab61eeee397acf763ced0264887438556f658
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-09 22:17:49 +0800

    init home.ejs and server port

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get('/', function (req, res) {
+  res.render(__dirname + '/views/home.ejs')
+})
 
 
 
@@ -27,7 +30,7 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
