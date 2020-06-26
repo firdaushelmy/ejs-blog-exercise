1e789044de18a433e866ee28ab6034ebca3e1ab1
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-26 23:28:26 +0800

    snapshot of problem encountered to set up database

--- app.js
+++ app.js
@@ -16,24 +16,26 @@ app.use(express.static("public"));
 
 const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(`${MONGO_URL}`, { useNewUrlParse = true, useUnifiedTopology = true });
+mongoose.connect(`${MONGO_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
-const postsSchema = new item({
-  post: {
+const postsSchema = ({
+  title: {
     type: String,
-    required = true,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
   },
 });
 
 const listsSchema = {
-  name = String,
-  posts =[postsSchema],
+  name: String,
+  posts: [postsSchema],
 }
 
 const Post = mongoose.model('post', postsSchema);
@@ -69,13 +71,36 @@ app.get('/compose', function (req, res) {
   res.render('compose')
 })
 
+// stopped here because i dont know why this is not working
+// try out to dolist on localhost
 app.post('/compose', function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    body: req.body.postBody,
-  }
-  posts.push(post);
-  res.redirect('/');
+  // const post = {
+  //   title: req.body.postTitle,
+  //   body: req.body.postBody,
+  // }
+  // posts.push(post);
+  // res.redirect('/');
+
+  List.findOne({ name: 'BlogMongo' }, function (err, foundMongo) {
+    // if (!foundMongo) {
+    //   const list = new list({
+    //     name: 'BlogMongo'
+    //   })
+    //   list.save()
+    //   res.redirect('/compose');
+    // } else {
+
+    if (foundMongo) {
+      const post = new post({
+        title: req.body.postTitle,
+        content: req.body.postBody,
+      });
+      List.posts.push(post)
+      post.save();
+      res.redirect('/compose')
+    }
+    // }
+  })
 })
 
 port = process.env.PORT || 3000
